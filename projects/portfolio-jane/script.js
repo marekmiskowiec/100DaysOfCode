6843d9135256c0c76de8cd00b0a72c08bb37bb5e
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2026-02-05 14:31:10 +0100

    add basic mobile menu

---
+++ projects/portfolio-jane/script.js
@@ -0,0 +1,9 @@
+function displayMobileNav() {
+  let x = document.getElementById("myLinks");
+
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
